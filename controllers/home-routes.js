269a5d77424c72a0030137390be15144a759e61f
Chau Pham <ncp9988@gmail.com>
2022-02-11 09:00:23 +0700

    homepage rendered

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,46 +1,48 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // get all posts for dashboard
-router.get('/', withAuth, (req, res) => {
+router.get('/',  (req, res) => {
   console.log(req.session);
-  console.log('======================');
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  console.log('=========Home route=============');
+  res.render("homepage")
+  // Post.fll({
+
+  //   where: {
+  //     user_id: req.session.user_id
+  //   },
+  //   attributes: [
+  //     'id',
+  //     'post_url',
+  //     'title',
+  //     'created_at',
+  //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+  //   ],
+  //   include: [
+  //     {
+  //       model: Comment,
+  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+  //       include: {
+  //         model: User,
+  //         attributes: ['username']
+  //       }
+  //     },
+  //     {
+  //       model: User,
+  //       attributes: ['username']
+  //     }
+  //   ]
+  // })
+  //   .then(dbPostData => {
+  //     const posts = dbPostData.map(post => post.get({ plain: true }));
+  //     res.render('dashboard', { posts, loggedIn: true });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
